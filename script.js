63592d373bb1a19d8f2d6c618d07ca62939641d8
kiahbanerjee <banek015@newschool.edu>
2026-02-03 16:55:16 -0500

    text

--- script.js
+++ script.js
@@ -4,13 +4,22 @@ const aboutBtn = document.getElementById('aboutBtn');
 const aboutBox = document.getElementById('aboutBox');
 const projectsBtn = document.getElementById('projectsBtn'); 
 const workBoxes = document.getElementById('workBoxes'); 
+const instagramText = document.getElementById('instagramText');  
+const contactText = document.getElementById('contactText'); 
+const cvText = document.getElementById('cvText'); 
+
 
 kiahBtn.addEventListener('click', () => {
   image.classList.toggle('show');
+  instagramText.classList.toggle('show'); 
 });
 aboutBtn.addEventListener('click', () => {  
   aboutBox.classList.toggle('show');
+  contactText.classList.toggle('show');
+  cvText.classList.toggle('show');
+
 });
 projectsBtn.addEventListener('click', () => { 
   workBoxes.classList.toggle('show');
 });
+
